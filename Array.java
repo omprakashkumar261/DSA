2590b3df6ec8b2b941ac9eca1526d24569170bbc
omprakashkumar261 <161987926+omprakashkumar261@users.noreply.github.com>
2025-02-05 08:31:21 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+package com.aimerz;
+
+public class Array {
+	 public static void main(String[]args) {
+//		 System.out.println("hello");
+		 int arr[]= {1,2,3,2,1};
+		 int n=arr.length;
+		 for(int i=0; i<n; i++) {
+			 System.out.print(arr[i]+" ");
+		 }
+	 }
+}
